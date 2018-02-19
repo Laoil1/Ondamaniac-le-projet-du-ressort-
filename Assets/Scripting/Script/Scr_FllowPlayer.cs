2a9cbab632b1a91cb7fb33f9bd2b9c70c334a329
Laoil <35740715+Laoil1@users.noreply.github.com>
2018-02-19 10:48:15 +0100

    Bonhomme

---
+++ Assets/Scripting/Script/Scr_FllowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_FllowPlayer : MonoBehaviour {
+	public Transform target;
+
+	void Update () {
+		FollowPlayer ();		
+	}
+
+	IEnumerator FollowPlayer (){
+		yield return new WaitForSeconds (2);
+
+		transform.position = target.position;
+
+		yield break;
+	}
+}
