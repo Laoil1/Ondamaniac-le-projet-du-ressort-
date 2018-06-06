c61ac4f385efc81486718843e1ae839b3073aaa1
Laoil <35740715+Laoil1@users.noreply.github.com>
2018-06-06 15:07:35 +0200

    qq

--- Assets/Scripting/Scr_DestructCube.cs
+++ Assets/Scripting/Scr_DestructCube.cs
@@ -1,8 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Scr_DestructCube : MonoBehaviour {
 
+
+	void OnCollisionEnter (Collision other){
+		print (other.gameObject.tag); 
+		if (other.gameObject.tag == "Player"){  
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+		} 
+	}
 
 }
---
+++ Assets/Scripting/Script/scr_level_manager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scr_level_manager : MonoBehaviour {
+
+	public string NameOfScene;
+
+
+	void OnTriggerEnter (Collider other){
+		print (NameOfScene); 
+		SceneManager.LoadScene (NameOfScene);
+
+	}
+}
